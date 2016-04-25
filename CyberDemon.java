6553e25ecef04fb5e704e3434621b4c7bea04f87
Craig Becker <craigbecker@myself.com>
2016-04-25 04:06:11 -0400

    War is Functioning

--- CyberDemon.java
+++ CyberDemon.java
@@ -8,26 +8,23 @@
 public class CyberDemon extends Demon
 {
     // instance variables - replace the example below with your own
-    private int x;
+    
 
     /**
      * Constructor for objects of class CyberDemon
      */
     public CyberDemon()
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        super();
+        hp = rand.nextInt(35)+1;
+        strength = rand.nextInt(25)+1;
+        while (hp < 5)
+        {
+            hp = rand.nextInt(22)+1;
+        }
+        while (strength < 5)
+        {
+            strength = rand.nextInt(14)+1;
+        }
     }
 }
--- War.java
+++ War.java
@@ -12,6 +12,7 @@ public class War
     ArrayList<Creature> Allies = new ArrayList<Creature>();
     ArrayList<Creature> Enemies = new ArrayList<Creature>();
     Random rand = new Random();
+    private int dmgholder;
     private int coin;
     private int armyMax = 0;
     
@@ -28,7 +29,55 @@ public class War
         Allies.trimToSize();
         Enemies.trimToSize();
         
+        System.out.println("There are " + Allies.size() + " to fight for the side of good.");
+        System.out.println("To fight the likes of " + Enemies.size() + " demons!");
         
+        Creature Champion = Allies.get(0);
+        Creature Abyssal = Enemies.get(0);
+        while (Allies.size() > 1 && Enemies.size() > 1)
+        {
+            if(Champion.getClass() == Elf.class)
+            {
+                System.out.println("IM A ELF");
+            }
+            else if (Champion.getClass() == Human.class)
+            {
+                System.out.println("I AM A HUMAN");
+            }
+            dmgholder = Champion.damage();
+            Abyssal.hp -= dmgholder;
+            System.out.println("The champion for the good has dealt " + dmgholder + " damage to the enemy!");
+            
+            dmgholder = Abyssal.damage();
+            Champion.hp -= dmgholder;
+            System.out.println("The enemy has dealt " + dmgholder + " to the champion!");
+            
+            if (Champion.hp <= 0)
+            {
+                Allies.remove(0);
+                Champion = Allies.get(0);
+                System.out.println("A champion has fallen!");
+            }
+            else if (Abyssal.hp <= 0)
+            {
+                Enemies.remove(0);
+                Abyssal = Enemies.get(0);
+                System.out.println("The enemy has fallen!");
+            }
+        }
+        
+        if (Allies.size() == 1)
+        {
+            System.out.println("Humanity has fallen");
+        }
+        else if (Enemies.size() == 1)
+        {
+            System.out.println("Humanity has prevailed");
+        }
+        else
+        {
+            System.out.println("ERROR");
+        }
     }
     
     public int setMaxArmy()
@@ -61,9 +110,17 @@ public class War
     public void makeEnemies()
     {
         setMaxArmy();
-        while (Enemies.size() <= armyMax)
-        {
-            Enemies.add(new Balrog());
+        while (Enemies.size() <= armyMax){ 
+            coin = rand.nextInt(2)+1;
+            if (coin ==(1))
+            {
+                Enemies.add(new Balrog());
+            }
+                
+            else if(coin ==(2))
+            {
+                Enemies.add(new CyberDemon());
+            }
         }
     }
 }
