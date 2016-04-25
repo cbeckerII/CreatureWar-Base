8fe5095fdcde98638fb393bda83f09552f5ac801
Craig Becker <craigbecker@myself.com>
2016-04-25 02:59:55 -0400

    Demon_Starred

--- Demon.java
+++ Demon.java
@@ -7,27 +7,12 @@
  */
 public class Demon extends Creature
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
     /**
      * Constructor for objects of class Demon
      */
     public Demon()
     {
-        // initialise instance variables
-        x = 0;
+        super();
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
